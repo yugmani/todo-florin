783e7569685d91956c2efa0a911734462950b895
Himalaya <yugmani@yahoo.com>
2022-02-08 00:19:16 -0800

    local storage set

--- script.js
+++ script.js
@@ -5,23 +5,43 @@ const todos = document.getElementById("todos");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
+  addTodo();
+});
+
+function addTodo() {
   const todoText = input.value;
 
   if (todoText) {
     const todoEl = document.createElement("li");
     todoEl.innerText = todoText;
-    todos.appendChild(todoEl);
+    todos.prepend(todoEl);
 
     todoEl.addEventListener("click", () => {
       todoEl.classList.toggle("completed");
+      updateLS();
     });
 
     todoEl.addEventListener("contextmenu", (e) => {
       e.preventDefault();
 
       todoEl.remove();
+      updateLS();
     });
 
     input.value = "";
+    updateLS();
   }
-});
+}
+
+function updateLS() {
+  const todosEl = document.querySelectorAll("li");
+  const todos = [];
+  todosEl.forEach((todo) => {
+    todos.push({
+      text: todo.innerText,
+      completed: todo.classList.contains("completed"),
+    });
+  });
+
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
