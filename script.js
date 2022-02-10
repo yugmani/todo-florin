da73f391923dfb0b5bdcd27f659ae2837a3f3f18
Himalaya <yugmani@yahoo.com>
2022-02-09 23:54:24 -0800

    get items from local storage

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const form = document.getElementById("form");
 const input = document.getElementById("input");
-const todos = document.getElementById("todos");
+const todosUl = document.getElementById("todos");
+
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+if (todos) {
+  todos.forEach((todo) => {
+    addTodo(todo);
+  });
+}
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -8,13 +16,22 @@ form.addEventListener("submit", (e) => {
   addTodo();
 });
 
-function addTodo() {
-  const todoText = input.value;
+function addTodo(todo) {
+  let todoText = input.value;
+
+  if (todo) {
+    todoText = todo.text;
+  }
 
   if (todoText) {
     const todoEl = document.createElement("li");
     todoEl.innerText = todoText;
-    todos.prepend(todoEl);
+
+    if (todo && todo.completed) {
+      todoEl.classList.add("completed");
+    }
+
+    todosUl.prepend(todoEl);
 
     todoEl.addEventListener("click", () => {
       todoEl.classList.toggle("completed");
